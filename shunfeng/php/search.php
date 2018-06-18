997b6ac7e437ad4c9e0d5314901c916fcc7e1a33
yutianyudaodao <1396845172@qq.com>
2018-06-18 14:50:30 +0800

    ok

--- shunfeng/php/search.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	header('content-type:text/html;charset=utf-8');
-// 
-	$content=file_get_contents("https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd=啊&sid=1421_21114_26350_20930&req=2&cb=hehe");
-	// $content=file_get_contents("http://www.kuitao8.com/api/joke");
-	echo json_encode($content)
- ?>
--- shunfeng/src/script/js/cart_main.js
+++
@@ -1,9 +0,0 @@
-require.config({
-	baseUrl:"../script/lib/",
-	paths:{
-		"jquery":"../script/lib/jquery.min.js",
-		"jqcookie":"http://apps.bdimg.com/libs/jquery.cookie/1.4.1/jquery.cookie",
-	}
-})
-
-require(["../script/js/cart.js","../script/js/cart_lunbo.js"]);
